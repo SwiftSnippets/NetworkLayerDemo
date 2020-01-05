50b73a3ea6ed16c8547dc864282b924d8446e8e3
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2020-01-05 02:21:42 -0800

    add ApiError Enum

--- NetworkLayerDemo/Networking/Constants.swift
+++ NetworkLayerDemo/Networking/Constants.swift
@@ -30,4 +30,11 @@ struct Constants {
     enum ContentType: String {
         case json = "application/json"
     }
+    
+    enum ApiError: Error {
+        case forbidden              //Status code 403
+        case notFound               //Status code 404
+        case conflict               //Status code 409
+        case internalServerError    //Status code 500
+    }
 }
