00ccf091d889ac4e83cd0b7d83058360e4874384
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2020-01-05 03:47:11 -0800

    add api client and test response

--- NetworkLayerDemo/Networking/Constants.swift
+++ NetworkLayerDemo/Networking/Constants.swift
@@ -31,10 +31,4 @@ struct Constants {
         case json = "application/json"
     }
     
-    enum ApiError: Error {
-        case forbidden              //Status code 403
-        case notFound               //Status code 404
-        case conflict               //Status code 409
-        case internalServerError    //Status code 500
-    }
 }
--- NetworkLayerDemo/ViewController.swift
+++ NetworkLayerDemo/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import RxSwift
+
 
 class ViewController: UIViewController {
+    
+    //Dispose bag
+     private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        ApiClient.getPosts(userId: 1)
+                 .observeOn(MainScheduler.instance)
+                 .subscribe(onNext: { postsList in
+                     print("List of posts:", postsList)
+                 }, onError: { error in
+                     switch error {
+                     case ApiError.conflict:
+                         print("Conflict error")
+                     case ApiError.forbidden:
+                         print("Forbidden error")
+                     case ApiError.notFound:
+                         print("Not found error")
+                     default:
+                         print("Unknown error:", error)
+                     }
+                 })
+                 .disposed(by: disposeBag)
     }
 
 
